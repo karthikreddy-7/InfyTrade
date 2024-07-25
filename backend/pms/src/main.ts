858a0c54b101ae0c2e6adf097d1ff447d33c9669
karthikreddy-7 <basupallykarthikreddy@gmail.com>
2024-07-25 14:18:06 +0530

    resolved cors error

--- backend/pms/src/main.ts
+++ backend/pms/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: '*', // You can specify allowed origins here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
 
   // Create Swagger options
   const options = new DocumentBuilder()
