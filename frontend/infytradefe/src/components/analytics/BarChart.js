dd7d054ab090e647452c690ec52cb8258361f27b
karthikreddy-7 <basupallykarthikreddy@gmail.com>
2024-07-26 19:38:40 +0530

    added analytics code structure

---
+++ frontend/infytradefe/src/components/analytics/BarChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BarChart() {
+  return (
+    <>
+      <div>barChart</div>
+    </>
+  );
+}
+
+export default BarChart;
---
+++ frontend/infytradefe/src/components/analytics/LineChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LineChart() {
+  return (
+    <>
+      <div>lineChart</div>
+    </>
+  );
+}
+
+export default LineChartineChart;
