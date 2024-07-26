1899dafff87c2e8c8d0e6ad294b4a02ba85f7aaa
unknown <balram.ram748@gmail.com>
2024-07-26 13:33:47 +0530

    added signin and signup

--- frontend/infytradefe/src/components/signin.js
+++ frontend/infytradefe/src/components/signin.js
@@ -1,10 +1,103 @@
 import React from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+  Divider,
+} from "@mui/material";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+
+const clientId = "";
 
 const Signin = () => {
+  const handleGoogleLoginSuccess = (response) => {
+    console.log('Login Success:', response);
+  };
+
+  const handleGoogleLoginFailure = (error) => {
+    console.error('Login Failed:', error);
+  };
+
   return (
-    <>
-      <div>signin</div>
-    </>
+    <GoogleOAuthProvider clientId={clientId}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Typography variant="body2" color="textSecondary" sx={{ mt: 1 }}>
+            Please login to continue to your account
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Keep me logged in"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Divider sx={{ my: 2 }}>or</Divider>
+            <Box sx={{ mb: 2 }}>
+              <GoogleLogin
+                onSuccess={handleGoogleLoginSuccess}
+                onFailure={handleGoogleLoginFailure}
+                buttonText="Sign in with Google"
+              />
+            </Box>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Create one"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </GoogleOAuthProvider>
   );
 };
 
--- frontend/infytradefe/src/components/signup.js
+++ frontend/infytradefe/src/components/signup.js
@@ -1,10 +1,98 @@
-import React from "react";
+import React from 'react';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+  Divider,
+} from '@mui/material';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+
+const clientId = ' ';
 
 const Signup = () => {
+  const handleGoogleLoginSuccess = (response) => {
+    console.log('Login Success:', response);
+  };
+
+  const handleGoogleLoginFailure = (error) => {
+    console.error('Login Failed:', error);
+  };
+
   return (
-    <>
-      <div>signup</div>
-    </>
+    <GoogleOAuthProvider clientId={clientId}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Typography variant="body2" color="textSecondary" sx={{ mt: 1 }}>
+            Sign up to enjoy the features of InfyTrade
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Keep me logged in"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, bgcolor: 'primary.dark' }}
+            >
+              Sign Up
+            </Button>
+            <Divider sx={{ my: 2 }}>or</Divider>
+            <Box sx={{ mb: 2 }}>
+              <GoogleLogin
+                onSuccess={handleGoogleLoginSuccess}
+                onFailure={handleGoogleLoginFailure}
+                buttonText="Continue with Google"
+              />
+            </Box>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {'Already have an account? Sign in'}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </GoogleOAuthProvider>
   );
 };
 
