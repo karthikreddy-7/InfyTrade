68e5a6ca2b87ec1c595ab8d6724fadcd5d4a6320
karthikreddy-7 <basupallykarthikreddy@gmail.com>
2024-07-29 00:02:25 +0530

    refixed landingpage

--- frontend/infytradefe/src/components/footer.js
+++ frontend/infytradefe/src/components/footer.js
@@ -4,59 +4,53 @@ import store_logos from "../assests/store_logos.png";
 
 const Footer = () => {
   return (
-    <footer style={styles.footer}>
-      <div style={styles.container}>
-        <div style={styles.brand}>
-          <div style={{ display: "inline-block" }}>
-            <img src={logo} alt="InfyTrade Logo" style={styles.logo} />
-            <p style={styles.team}>Team Infy</p>
-          </div>
-          <div style={{ display: "inline-block" }}>
-            <p style={styles.tagline}>
-              Maximize your investment in the capital market world with the
-              convenience and various features provided by InfyTrade.
-            </p>
-          </div>
-          <div style={styles.badges}>
-            <button style={styles.storeButton}>
-              <img src={store_logos} alt="Stores" style={styles.store_logos} />
-            </button>
-          </div>
+    <footer className="footer footer-center bg-white text-primary-content p-10">
+      <aside>
+        <img src={logo} className="object-contain w-8 mr-2" alt="Logo" />
+        <p className="font-bold">
+          InfyTrade
+          <br />
+          Trade easy and earn largely
+        </p>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+      </aside>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+            </svg>
+          </a>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+            </svg>
+          </a>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
+          </a>
         </div>
-        <div style={styles.linksContainer}>
-          <div style={styles.linksColumn}>
-            <h4 style={styles.columnTitle}>Companies</h4>
-            <a href="/about" style={styles.link}>About Us</a>
-            <a href="/journey" style={styles.link}>Journey</a>
-            <a href="/blog" style={styles.link}>Blog</a>
-            <a href="/contact" style={styles.link}>Contact</a>
-            <a href="/help" style={styles.link}>Help</a>
-          </div>
-          <div style={styles.linksColumn}>
-            <h4 style={styles.columnTitle}>Resources</h4>
-            <a href="/about" style={styles.link}>About Us</a>
-            <a href="/journey" style={styles.link}>Journey</a>
-            <a href="/blog" style={styles.link}>Blog</a>
-            <a href="/contact" style={styles.link}>Contact</a>
-            <a href="/help" style={styles.link}>Help</a>
-          </div>
-          <div style={styles.linksColumn}>
-            <h4 style={styles.columnTitle}>Help</h4>
-            <a href="/house-rules" style={styles.link}>House Rules</a>
-            <a href="/terms" style={styles.link}>Our Terms</a>
-            <a href="/privacy" style={styles.link}>Privacy & Policy</a>
-          </div>
-          <div style={styles.contactColumn}>
-            <h4 style={styles.columnTitle}>Contact Us</h4>
-            <p style={styles.contactInfo}>+00 00000000</p>
-            <p style={styles.contactInfo}>Area 51, unknown</p>
-            <p style={styles.contactInfo}>johndoe@yahoo.com</p>
-          </div>
-        </div>
-      </div>
-      <div style={styles.copyright}>
-        <p>Copyright © InfyTrade 2024. All Rights Reserved.</p>
-      </div>
+      </nav>
     </footer>
   );
 };
@@ -64,14 +58,14 @@ const Footer = () => {
 const styles = {
   footer: {
     backgroundColor: "#F0F4F8",
-    paddingBottom: '0px',
+    paddingBottom: "0px",
     borderTop: "1px solid #e7e7e7",
     position: "relative",
     bottom: "0",
     width: "100%",
   },
   container: {
-    width: '100%',
+    width: "100%",
     paddingBottom: "0",
   },
   brand: {
@@ -81,14 +75,14 @@ const styles = {
     justifyContent: "space-between",
   },
   logo: {
-    height: "65px", 
+    height: "65px",
     display: "inline-block",
   },
   tagline: {
     margin: "10px 0",
     color: "#666",
     display: "inline-block",
-    fontSize: "20px", 
+    fontSize: "20px",
     lineHeight: "1.5",
   },
   team: {
--- frontend/infytradefe/src/components/landingPage.js
+++ frontend/infytradefe/src/components/landingPage.js
@@ -34,8 +34,8 @@ const LandingPage = () => {
 
   return (
     <>
-      <div className="text-left p-4 m-2 max-w-screen mx-auto bg-gray-100 rounded-lg">
-        <div className="flex flex-row h-screen max-w-screen">
+      <div className="text-left p-2 max-w-screen mx-auto bg-white rounded-lg">
+        <div className="flex flex-row mb-8 max-h-screen max-w-screen">
           <div className="w-3/5 p-2 text-2xl m-4 mt-16">
             <p className="text-blue-500 text-2xl font-bold mb-5">
               Trade Smarter with InfyTrade
@@ -68,138 +68,159 @@ const LandingPage = () => {
             />
           </div>
         </div>
-        
-        <h2 className="text-2xl font-bold text-blue-700 text-center mb-10">
+        <div className="divider"></div>
+        <h2 className="text-3xl font-bold leading-tight text-blue-600 text-center mb-10">
           Why choose InfyTrade?
         </h2>
-        <h3 className="text-blue-500 text-xl text-center mb-10">
+        <h3 className=" text-2xl font-bold text-center mb-10">
           Specially designed for stock market
         </h3>
-        <div className="flex justify-around flex-wrap">
-          <div className="w-1/3 m-5 p-5 border border-gray-300 rounded text-left">
-            <img
-              src={ui_customization}
-              alt="UI Customization"
-              className="w-4/5 h-auto mb-5"
-            />
-            <h4 className="text-xl font-bold mb-5">UI Customization</h4>
-            <p className="text-lg text-gray-700">
-              Easily personalize your trading interface with our intuitive
-              drag-and-drop options, making it easy to create a layout that
-              suits your needs.
-            </p>
+        <div className="flex justify-between flex-wrap rounded-md">
+          <div className="w-1/3 p-2 h-[80vh] rounded-md">
+            <div className="p-3 border border-gray-300 h-4/5 rounded text-left flex flex-col items-center">
+              <img
+                src={ui_customization}
+                alt="UI Customization"
+                className="w-4/5 h-auto "
+              />
+              <h4 className="text-lg font-bold m-3">UI Customization</h4>
+              <p className="text-md text-gray-700 font-sans font-semibold">
+                Easily personalize your trading interface with our intuitive
+                drag-and-drop options, making it easy to create a layout that
+                suits your needs.
+              </p>
+            </div>
           </div>
-          <div className="w-1/3 m-5 p-5 border border-gray-300 rounded text-left">
-            <img
-              src={rtdata_analysis}
-              alt="Real-Time Data Analytics"
-              className="w-4/5 h-auto mb-5"
-            />
-            <h4 className="text-xl font-bold mb-5">Real-Time Data Analytics</h4>
-            <p className="text-lg text-gray-700">
-              Leverage real-time data analytics with a variety of charts and
-              plots to gain deeper insights into market trends and make informed
-              trading decisions.
-            </p>
+          <div className="w-1/3 p-2">
+            <div className="p-3 border border-gray-300 h-4/5 rounded text-left flex flex-col items-center">
+              <img
+                src={rtdata_analysis}
+                alt="Real-Time Data Analytics"
+                className="w-4/5 h-auto mb-3"
+              />
+              <h4 className="text-lg font-bold mb-3">
+                Real-Time Data Analytics
+              </h4>
+              <p className="text-md text-gray-700 font-sans font-semibold">
+                Leverage real-time data analytics with a variety of charts and
+                plots to gain deeper insights into market trends and make
+                informed trading decisions.
+              </p>
+            </div>
           </div>
-          <div className="w-1/3 m-5 p-5 border border-gray-300 rounded text-left">
-            <img
-              src={mlmodels}
-              alt="Advanced ML Models"
-              className="w-4/5 h-auto mb-5"
-            />
-            <h4 className="text-xl font-bold mb-5">Advanced ML Models</h4>
-            <p className="text-lg text-gray-700">
-              Access a library of predefined ML model templates tailored for
-              stocks, forex, crypto, and futures & options, allowing you to
-              apply advanced analytics effortlessly.
-            </p>
+          <div className="w-1/3 p-2">
+            <div className="p-3 border border-gray-300 h-4/5 rounded text-left flex flex-col items-center">
+              <img
+                src={mlmodels}
+                alt="Advanced ML Models"
+                className="w-4/5 h-auto mb-3"
+              />
+              <h4 className="text-lg font-bold mb-3">Advanced ML Models</h4>
+              <p className="text-md text-gray-700 font-sans font-semibold">
+                Access a library of predefined ML model templates tailored for
+                stocks, forex, crypto, and futures & options, allowing you to
+                apply advanced analytics effortlessly.
+              </p>
+            </div>
           </div>
         </div>
-
-        <h2 className="text-2xl font-bold text-blue-700 text-center mt-10 mb-10">
+        <div className="divider"></div>
+        <h2 className="text-3xl font-bold text-blue-600 text-center mt-10">
           Additional advantages from InfyTrade
         </h2>
-        <div className="flex justify-between flex-wrap mt-10">
-          <div className="w-1/4 text-center p-5 bg-white border border-gray-300 rounded m-2">
+        <div className="flex justify-between flex-wrap rounded-md mt-8 p-2 gap-3 ">
+          <div className="w-[22vw] text-center p-4 bg-gray-100 border border-gray-300 rounded-md shadow-md">
             <img
               src={dynamicDashboards}
               alt="Dynamic Dashboards"
-              className="w-4/5 h-auto mb-5"
+              className="w-4/5 h-auto mb-3 mx-auto rounded-md"
             />
-            <h4 className="text-xl font-bold mb-5">Dynamic Dashboards</h4>
-            <p className="text-lg text-gray-700">
+            <h4 className="text-xl font-bold mb-3">Dynamic Dashboards</h4>
+            <p className="text-base text-gray-700 ">
               Create and share multiple dashboards, either publicly or
               privately, making it easy to track and analyze your trading
               performance.
             </p>
           </div>
-          <div className="w-1/4 text-center p-5 bg-white border border-gray-300 rounded m-2">
+          <div className="w-[22vw] text-center p-4 bg-gray-100 border border-gray-300 rounded-md shadow-md">
             <img
               src={traderRankings}
               alt="Trader Rankings"
-              className="w-4/5 h-auto mb-5"
+              className="w-4/5 h-auto mb-3 mx-auto rounded-md"
             />
-            <h4 className="text-xl font-bold mb-5">Trader Rankings</h4>
-            <p className="text-lg text-gray-700">
-              Learn from the best by viewing the rankings of top traders who
-              have opted to share their portfolios publicly, and enhance your
-              own trading skills.
+            <h4 className="text-xl font-bold mb-3">Trader Rankings</h4>
+            <p className="text-base text-gray-700 ">
+              Learn from best by viewing the rankings of top traders who have
+              opted to share their portfolios publicly, and enhance your trading
+              skills.
             </p>
           </div>
-          <div className="w-1/4 text-center p-5 bg-white border border-gray-300 rounded m-2">
+          <div className="w-[22vw] text-center p-4 bg-gray-100 border border-gray-300 rounded-md shadow-md">
             <img
               src={communityMarketplace}
               alt="Community and Marketplace"
-              className="w-4/5 h-auto mb-5"
+              className="w-4/5 h-auto mb-3 mx-auto rounded-md"
             />
-            <h4 className="text-xl font-bold mb-5">
+            <h4 className="text-xl font-bold mb-3">
               Community and Marketplace
             </h4>
-            <p className="text-lg text-gray-700">
+            <p className="text-base text-gray-700 ">
               Join a vibrant community of traders, make connections, and access
               a marketplace filled with resources, tools, and insights.
             </p>
           </div>
-          <div className="w-1/4 text-center p-5 bg-white border border-gray-300 rounded m-2">
+          <div className="w-[22vw] text-center p-4 bg-gray-100 border border-gray-300 rounded-md shadow-md">
             <img
               src={invention}
               alt="Invention"
-              className="w-4/5 h-auto mb-5"
+              className="w-4/5 h-auto mb-3 mx-auto rounded-md"
             />
-            <h4 className="text-xl font-bold mb-5">Invention</h4>
-            <p className="text-lg text-gray-700">
-              InfyTrade is the result of the production, refinement, or
-              development of the original invention. It is also continuous and
-              continuously developing in a better direction.
+            <h4 className="text-xl font-bold mb-3">Invention</h4>
+            <p className="text-base text-gray-700 ">
+              InfyTrade is the result of the development of the original
+              invention.continuous and continuously developing in a better
+              direction.
             </p>
           </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center text-center mt-10 mb-10 p-10 bg-gray-200 rounded">
-          <div className="mb-5">
-            <h2>Stay updated with our weekly newsletter</h2>
-            <p>
-              a collection of the hottest stock market news sent every week to
+        <div className="flex flex-row items-center justify-center mt-10 h-[70vh]">
+          <div className="w-1/2 ">
+            <h2 className="font-bold text-3xl">Stay updated</h2>
+            <h2 className="font-bold text-3xl mt-4">
+              with our weekly newsletter
+            </h2>
+            <p className=" mt-4">
+              A collection of the hottest stock market news sent every week to
               your email
             </p>
           </div>
-          <div className="flex flex-col items-center w-full max-w-xs">
-            <input
-              type="email"
-              placeholder="Email address"
-              className="w-full p-2 mb-2 border border-gray-400 rounded"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full p-2 mb-2 border border-gray-400 rounded"
-            />
-            <button
-              className="bg-blue-500 text-white border-none p-3 rounded cursor-pointer text-xl"
-              onClick={handleNewsletterSignup}
-            >
-              Get Started
+          <div className="flex flex-col items-center w-full max-w-xs mt-16 gap-4">
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input type="text" className="grow" placeholder="Email" />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+              </svg>
+              <input type="text" className="grow" placeholder="Name" />
+            </label>
+            <button className="btn btn-primary btn-wide text-white mt-6">
+              Get Notified
             </button>
           </div>
           {subscriptionMessage && (
